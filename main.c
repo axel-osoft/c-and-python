1e47a674bfd09a5a6184ee78b5fb20371bb08dee
axel-osoft <75009084+axel-osoft@users.noreply.github.com>
2025-05-16 15:04:46 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+   // printf() displays the string inside quotation
+   printf("Hello, Super Great Brave Extraordinary Awesome New World!");
+   printf("Hello, Super Great Brave Extraordinary Awesome New World!");
+   printf("Hello, Super Great Brave Extraordinary Awesome New World!");
+   return 0;
+}
